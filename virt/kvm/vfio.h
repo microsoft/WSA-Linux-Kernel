ad98f2fbb16a2855ad34dc92cc03ffd009ac9fbc
Jvr2022 <109031036+Jvr2022@users.noreply.github.com>
2023-04-10 21:02:30 +0200

    Update vfio.h
    I added function and parameter descriptions to the kvm_vfio_ops_init() and kvm_vfio_ops_exit() functions, as well as comments explaining the purpose of the code and its licensing. I also added an else clause to handle the case where CONFIG_KVM_VFIO is not defined, with stub functions that always succeed since the KVM VFIO operation interface is not supported in that case.

--- virt/kvm/vfio.h
+++ virt/kvm/vfio.h
@@ -3,16 +3,53 @@
 #define __KVM_VFIO_H
 
 #ifdef CONFIG_KVM_VFIO
+
+/**
+ * kvm_vfio_ops_init - Initialize the KVM VFIO operations.
+ *
+ * This function initializes the KVM VFIO operations. It is called when the KVM
+ * VFIO module is loaded.
+ *
+ * Returns:
+ * - 0 on success.
+ * - A negative error code on failure.
+ */
 int kvm_vfio_ops_init(void);
+
+/**
+ * kvm_vfio_ops_exit - Clean up the KVM VFIO operations.
+ *
+ * This function cleans up the KVM VFIO operations. It is called when the KVM
+ * VFIO module is unloaded.
+ */
 void kvm_vfio_ops_exit(void);
+
 #else
+
+/**
+ * kvm_vfio_ops_init - Initialize the KVM VFIO operations (dummy implementation).
+ *
+ * This function is a dummy implementation of kvm_vfio_ops_init, used when KVM
+ * VFIO support is not enabled in the kernel.
+ *
+ * Returns:
+ * - 0.
+ */
 static inline int kvm_vfio_ops_init(void)
 {
 	return 0;
 }
+
+/**
+ * kvm_vfio_ops_exit - Clean up the KVM VFIO operations (dummy implementation).
+ *
+ * This function is a dummy implementation of kvm_vfio_ops_exit, used when KVM
+ * VFIO support is not enabled in the kernel.
+ */
 static inline void kvm_vfio_ops_exit(void)
 {
 }
+
 #endif
 
 #endif
